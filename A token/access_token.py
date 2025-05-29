c9a669ea41863fca40bf61cacd423bbcf8462a7b
OmkarsinhSolanki <somkarsinh@gmail.com>
2025-05-29 23:04:51 +0530

    New Updates

--- A token/access_token.py
+++ A token/access_token.py
@@ -6,12 +6,10 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-# do every time
-# https://kite.trade/connect/login?api_key=sga0oscku88h6hhi&v=3
-# use the link ginen above to get new 'request token" and ureplace it with your_request_token down here.
 
-kite = KiteConnect(api_key="sga0oscku88h6hhi")
-data = kite.generate_session("Ozg0DmhRCY1AAEqcaaSaY1KpUFu4YSb5", api_secret="3sxovyrdcawudyy33f6yjom964x759tw")
+
+kite = KiteConnect(api_key=API_KEY)
+data = kite.generate_session("Ozg0DmhRCY1AAEqcaaSaY1KpUFu4YSb5", api_secret=API_SECRET)
 access_token = data["access_token"]
 
 print("Access Token:", access_token)
