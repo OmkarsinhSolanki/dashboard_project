ce98d8129ee95fe78e569619a71afb96c0c165cf
OmkarsinhSolanki <somkarsinh@gmail.com>
2025-05-30 18:34:06 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,16 +1,9 @@
 import streamlit as st
-from stocks_filter import main as box6_main
-from value_editor import main as value_editor_main
-
 
 # streamlit run main_app.py
 
 # Configure page
-st.set_page_config(
-    page_title="Stock Analysis Dashboard", 
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
+st.set_page_config(page_title="Stock Analysis Dashboard", layout="wide", initial_sidebar_state="expanded")
 
 # Custom CSS to hide the default Streamlit menu and footer
 hide_st_style = """
@@ -22,18 +15,29 @@ hide_st_style = """
 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
+
 # Sidebar navigation
 st.sidebar.title("Stocks Analysis")
 st.sidebar.markdown("---")
+
 app_mode = st.sidebar.radio(
     "Navigation",
-    ["📊 Dashboard", "✏️ Value Editor"],
+    ["📊 Dashboard", "✏️ Value Editor", "🔑 Token Generator"],
     index=0
 )
 
-# Main app logic
+
+# main app
+from value_editor import main as value_editor_main
+if app_mode == "✏️ Value Editor":
+    value_editor_main()
+
+from token_generator import main as token_generator_main
+if app_mode == "🔑 Token Generator":
+    token_generator_main()
+
+from stocks_filter import main as box6_main
 if app_mode == "📊 Dashboard":
     box6_main()
-else:
-    value_editor_main()
+
 
